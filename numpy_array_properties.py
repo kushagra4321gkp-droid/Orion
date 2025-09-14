36d548a8bc9de5f533258e2711fd2102b6956ad1
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-09-14 13:59:13 +0530

    aggregation_func

--- numpy_array_properties.py
+++ numpy_array_properties.py
@@ -9,3 +9,15 @@ print(arr_2D.size)
 print("printing dimension below")
 find_ndim = np.ndim(arr_2D)
 print(find_ndim)
+
+print("4 dimensional array")
+arr_4D = np.array([[[[1,2],[3,4],[5,'savita'],[7,8]]]])
+# print(arr_4D.shape)
+# print(arr_4D.size)
+print(arr_4D.ndim)
+
+#dtype is used to tell datatype of the elements present in the array
+print(arr_4D.dtype)
+
+arr_4D.astype(str)
+print(arr_4D.dtype)
